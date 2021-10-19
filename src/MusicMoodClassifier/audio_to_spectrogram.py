dfd3a8c36869020b3cf064db207a149d45893ab4
Lokyin <1340619187@qq.com>
2021-10-19 17:17:45 +0800

    初步完成分类器

---
+++ src/MusicMoodClassifier/audio_to_spectrogram.py
@@ -0,0 +1,64 @@
+from pydub import AudioSegment
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Audio2Spectrogram:
+    """
+    Convert the sound signal of music into a spectrogram
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def audio_to_wav(src, dest):
+        """Convert the mp3 audio file into wav format.
+
+        Args:
+            src: the source file to be converted, mp3 format expected.
+            dest: the target wav file to be created.
+
+        Returns:None
+
+        """
+        audio = AudioSegment.from_mp3(src)
+        audio.export(dest, format='wav')
+
+    @staticmethod
+    def wav_to_spectrogram(src, dest):
+        """Convert wav audio to spectrogram file
+
+        Args:
+            src:the source file to be converted, wav format expected.
+            dest:the target spectrogram to be created, in jpg format.
+
+        Returns: None
+
+        """
+        y, sr = librosa.load(src)
+        S = librosa.feature.melspectrogram(y=y, sr=sr)
+
+        x_pixels = 384
+        y_pixels = 128
+        plt.figure(figsize=(x_pixels / 100, y_pixels / 100))
+        plt.axis('off')
+        # add an axes
+        plt.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[])
+
+        librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', x_axis='time')
+
+        plt.show()
+        plt.savefig(dest, bbox_inches=None, pad_inches=0, dpi=100)
+        plt.close()
+
+
+if __name__ == '__main__':
+    mp3_file = '../data/mp3/cls.mp3'
+    wav_file = '../data/wav/cls.wav'
+    spec_file = '../data/spectrogram/cls.jpg'
+    audio2spectrogram = Audio2Spectrogram()
+    # audio2spectrogram.audio_to_wav(mp3_file, wav_file)
+    audio2spectrogram.wav_to_spectrogram(wav_file, spec_file)
---
+++ src/MusicMoodClassifier/predictor.py
@@ -0,0 +1,67 @@
+import torch
+import torchvision
+
+from PIL import Image
+
+
+# from torchvision import models, transforms
+
+
+class Predictor:
+    """
+    Predict the music mood by the input spectrogram
+    """
+
+    def __init__(self, trained_weights_path):
+        """
+        Initialize the Predictor by the trained data information
+
+        Args:
+            trained_weights_path: the trained weights data file path
+        """
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.model = torchvision.models.resnet18()
+        # Change the last layer shape
+        num_features = self.model.fc.in_features
+        self.model.fc = torch.nn.Linear(num_features, 4)
+        # Put trained weights
+        self.model.load_state_dict(torch.load(trained_weights_path, map_location=torch.device(self.device)))
+        self.model.eval()
+        # Mood class names
+        self.class_names = ["angry", "happy", "relaxed", "sad"]
+        # Compose transformers and normalize it
+        self.loader = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                                                      torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                                       std=[0.229, 0.224, 0.225])])
+
+    @staticmethod
+    def map_from_idx(array, map_list):
+        """
+        Function that takes an array of indices positions and returns
+        the elements at these specific positions in map_list
+        """
+        res = []
+        for i in array:
+            res.append(map_list[int(i)])
+        return res
+
+    def predict(self, img_path, k=2):
+        """
+        Predict the music mood by it's spectrogram
+
+        Args:
+            img_path: test image file, .jep format expected
+            k: top k
+
+        Returns:
+            the predicted mood class and its corresponding probability
+        """
+        img = Image.open(img_path)
+        img = self.loader(img).unsqueeze(0).to(self.device)
+        self.model.eval()
+        output = self.model(img)
+        probability, class_idx = torch.topk(output, k)
+        probability = probability[0].detach().numpy()
+        class_idx = class_idx[0].detach().numpy()
+        class_val = self.map_from_idx(class_idx, self.class_names)
+        return class_val, probability
