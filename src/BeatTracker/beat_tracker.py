33219489e05b13e527eff74586af0cae8012daf8
Lokyin <1340619187@qq.com>
2021-11-02 15:36:57 +0800

    Optimized vibration module

--- src/BeatTracker/beat_tracker.py
+++ src/BeatTracker/beat_tracker.py
@@ -1,4 +1,7 @@
 import librosa.beat
+import librosa.display
+from sklearn import preprocessing
+import numpy as np
 
 
 class BeatTracker:
@@ -15,9 +18,16 @@ class BeatTracker:
         """Get the beats information of a track
 
         @param music_src: music source file path
-        @return: estimated beat event locations in time units
+        @return: estimated beat event locations in time units, scaled beats strength, mean of strength
         """
+        # set sampling rate to 48kHz
         y, sr = librosa.load(music_src)
-        tempo, track_beats = librosa.beat.beat_track(y=y, sr=sr, units='time')
-        return track_beats
-
+        tempo, track_beats_time = librosa.beat.beat_track(y=y, sr=sr, units="time")
+        beats_track = []
+        for beat_time in track_beats_time:
+            # 对前后0.1s的数据进行平均
+            beats_track.append(np.abs(y[int((beat_time - 0.1) * sr):int((beat_time + 0.1) * sr)]).mean())
+        beats_track = np.array(beats_track)
+        scaled_beats_track = [(i - beats_track.min()) / (beats_track.max() - beats_track.min()) for i in beats_track]
+        print(np.mean(scaled_beats_track))
+        return track_beats_time, scaled_beats_track, np.mean(scaled_beats_track)
